104c578edde9d2ca1a31bb6fd177c1233a88d8bb
Fabio Junior Oliveira Santos <fabio.santos@perkons.com>
2025-03-02 20:47:21 -0300

    alterado functions

--- netlify/edge-functions/meta-tags.ts
+++ netlify/edge-functions/meta-tags.ts
@@ -18,6 +18,7 @@ export default async (request: Request, context: Context): Promise<Response> => 
   const id = pathSegments[pathSegments.length - 1];
 
   const userAgent = request.headers.get("User-Agent") || "";
+  console.log(request.headers.get("User-Agent"));
   const isCrawler = isBot(userAgent);
 
   let title = "Meu Site";
@@ -34,7 +35,7 @@ export default async (request: Request, context: Context): Promise<Response> => 
     if (data) {
       title = data.nome || title;
       description = "As melhores promoções, cupons e descontos das maiores lojas do Brasil - São dezenas de Promoções postadas postadas diariamente!";
-      image = data.imagem || image;
+      image = data.image || image;
     }
   }
 
