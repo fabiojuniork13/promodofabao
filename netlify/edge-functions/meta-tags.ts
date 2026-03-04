b1714ecb92c1086d778ca1c3b72c706dd71ec104
Fabio Junior Oliveira Santos <fabio.santos@perkons.com>
2026-03-04 20:01:27 -0300

    melhoria meta tags retirado type

--- netlify/edge-functions/meta-tags.ts
+++ netlify/edge-functions/meta-tags.ts
@@ -45,7 +45,6 @@ export default async (request: Request, context: Context): Promise<Response> => 
         <meta property="og:image:width" content="752">
         <meta property="og:image:height" content="750">
         <meta property="og:type" content="website">
-        <meta property="og:image:type" content="image/jpeg
         <meta property="og:url" content="${url.href}">
       </head>
       <body>
